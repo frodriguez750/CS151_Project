f30a082d1ae5d198a2e271a38191adbfb2eeb776
Frodriguez1 <fidel.rodriguez@sjsu.edu>
2024-12-08 19:17:12 -0800

    initial commit "initial commit"

---
+++ src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Cashier App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		InventoryPanel inventoryPanel = new InventoryPanel();
+		frame.add(inventoryPanel);
+		InvoicePanel invoicePanel = new InvoicePanel();
+		frame.add(invoicePanel);
+		frame.setVisible(true);
+
+		
+	}
+}
